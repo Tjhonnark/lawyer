b96cff266f339409a05d7ba9fb7451f3cd9a8cb6
Thomaz <tjhonnar@gmail.com>
2022-04-11 18:22:50 -0500

    vs1.13

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,26 @@
+import Head from 'next/head';
 import Navbar from './Navbar';
 import Contact from './Contact';
 
 export default function Layout(props){
     return (
         <>
-            <header>
-                <Navbar />
-            </header>
+            <Head>
+                <title>RGarcia Abogado</title>
+                <meta name="description" content="Abogado y Consultor que ofrece servicios legales a personas y empresas nacionales e internacionales." />
+
+                {/* FACEBOOK */}
+                <meta property="og:title" content="RGarcia Abogado" />
+                <meta property="og:description" content="Abogado y Consultor que ofrece servicios legales a personas y empresas nacionales e internacionales." />
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="rgarciabogado.ga" />
+                <meta property="og:image" content="" />
+                {/* TWITTER */}
+                <meta name="twitter:card" content="summary_large_image"/>
+                <meta name="twitter:title" content="RGarcia Abogado"/>
+                <meta name="twitter:creator" content=""/>
+            </Head>
+            <Navbar />
             <main className=''>
                 {props.children}
             </main>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,21 +11,6 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                <head>
-                    <title>RGarcia Abogado</title>
-                    <meta name="description" content="Abogado y Consultor que ofrece servicios legales a personas y empresas nacionales e internacionales." />
-
-                    {/* FACEBOOK */}
-                    <meta property="og:title" content="RGarcia Abogado" />
-                    <meta property="og:description" content="Abogado y Consultor que ofrece servicios legales a personas y empresas nacionales e internacionales." />
-                    <meta property="og:type" content="website" />
-                    <meta property="og:url" content="rgarciabogado.ga" />
-                    <meta property="og:image" content="" />
-                    {/* TWITTER */}
-                    <meta name="twitter:card" content="summary_large_image"/>
-                    <meta name="twitter:title" content="RGarcia Abogado"/>
-                    <meta name="twitter:creator" content=""/>
-                </head>
                     <script
                         dangerouslySetInnerHTML={{
                             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
