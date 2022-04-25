fe7e937f10df569250f7bff6feea849e730eb75b
Thomaz <tjhonnar@gmail.com>
2022-04-25 12:22:57 -0500

    vs1.2.0

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2f527ee9dc050d77"; }
+/******/ 		__webpack_require__.h = function() { return "ff454608dbc37a43"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
