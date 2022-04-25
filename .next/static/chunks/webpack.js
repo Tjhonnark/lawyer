90a1f320d3cc4202502cfa125ec367910565c0fe
Thomaz <tjhonnar@gmail.com>
2022-04-25 13:46:12 -0500

    vs1.2.2

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "add73afdd5f1b6d4"; }
+/******/ 		__webpack_require__.h = function() { return "f1093ddb5f602136"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
